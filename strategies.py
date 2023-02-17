fbe9db1b5259e2abc2944d16165e88d410b56e3f
Pepijn <99075810+PepijnDevue@users.noreply.github.com>
2023-02-17 11:20:48 +0100

    Added comments and docstring to strategies.py

--- strategies.py
+++ strategies.py
@@ -1,7 +1,14 @@
-import itertools
+# general imports
+from itertools import product
 
+# file imports
 from visuals import colours
 
 def make_combinations():
-    combinations = list(itertools.product(colours, repeat=4))
+    """
+    makes a list containing all possible colour combinations in the form of tuples
+
+    return: the list of tuples
+    """
+    combinations = list(product(colours, repeat=4)) # use itertools.product to get a list of all possible combinations of 4 characters long only containing the six colours
     return combinations
